e8aa997940f1bd43da4031309a4e536194db3236
desmax74 <mdessi@redhat.com>
2025-07-15 16:56:09 +0200

    FIx install mode, icon
    Signed-off-by: desmax74 <mdessi@redhat.com>

--- generate-fbc.sh
+++ generate-fbc.sh
@@ -75,7 +75,7 @@ LABEL operators.operatorframework.io.bundle.package.v1=rhtpa-operator
 LABEL operators.operatorframework.io.bundle.channels.v1=stable,stable-v1.0
 LABEL operators.operatorframework.io.metrics.builder=operator-sdk-v1.34.1
 LABEL operators.operatorframework.io.metrics.mediatype.v1=metrics+v1
-LABEL operators.operatorframework.io.metrics.project_layout=go.kubebuilder.io/v4
+LABEL operators.operatorframework.io.metrics.project_layout=hybrid.helm.sdk.operatorframework.io/v1-alpha
 LABEL operators.operatorframework.io.index.configs.v1=/configs
 
 EOT
